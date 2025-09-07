a17472dfaf4ad4d5967bce9cd13c35ea0541b746
adedeni <adeshinaolayodemubaraq@gmail.com>
2025-09-07 09:03:18 +0100

    linked name on the appbar with settings screen

--- lib/features/home/screens/widgets/home_appbar.dart
+++ lib/features/home/screens/widgets/home_appbar.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ushopia/features/personalization/screens/settings/settings_screen.dart';
 import '/features/shop/screens/cart/cart.dart';
 
 import '/../common/widgets/appbar/appbar.dart';
@@ -8,31 +9,32 @@ import '/../utilities/constants/colors.dart';
 import '/../utilities/constants/text_strings.dart';
 
 class AHomeAppBar extends StatelessWidget {
-  const AHomeAppBar({
-    super.key,
-  });
+  const AHomeAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return AAppBar(
-      title: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            ATexts.homeAppbarTitle,
-            style: Theme.of(context)
-                .textTheme
-                .labelMedium!
-                .apply(color: AColors.grey),
-          ),
-          Text(
-            ATexts.homeAppbarSubTitle,
-            style: Theme.of(context)
-                .textTheme
-                .headlineSmall!
-                .apply(color: AColors.white),
-          ),
-        ],
+      title: GestureDetector(
+        onTap: () {
+          Get.to(() => SettingsScreen());
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              ATexts.homeAppbarTitle,
+              style: Theme.of(
+                context,
+              ).textTheme.labelMedium!.apply(color: AColors.grey),
+            ),
+            Text(
+              ATexts.homeAppbarSubTitle,
+              style: Theme.of(
+                context,
+              ).textTheme.headlineSmall!.apply(color: AColors.white),
+            ),
+          ],
+        ),
       ),
       actions: [
         ACartCounterIcon(
