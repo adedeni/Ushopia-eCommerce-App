b824c42b3db3b92337d5eef33415effeb6008978
adedeni <adeshinaolayodemubaraq@gmail.com>
2025-09-08 12:27:18 +0100

    fix bug in custom curve edge widget

--- lib/common/widgets/custom_shapes/curve_edges/curve_edges.dart
+++ lib/common/widgets/custom_shapes/curve_edges/curve_edges.dart
@@ -8,19 +8,30 @@ class ACustomCurveEdges extends CustomClipper<Path> {
 
     final firstLeftCurve = Offset(0, size.height - 20);
     final firstRightCurve = Offset(30, size.height - 20);
-    path.quadraticBezierTo(firstLeftCurve.dx, firstLeftCurve.dy,
-        firstRightCurve.dx, firstRightCurve.dy);
+    path.quadraticBezierTo(
+      firstLeftCurve.dx,
+      firstLeftCurve.dy,
+      firstRightCurve.dx,
+      firstRightCurve.dy,
+    );
 
     final secondLeftCurve = Offset(0, size.height - 20);
     final secondRightCurve = Offset(size.width - 30, size.height - 20);
-    path.quadraticBezierTo(secondLeftCurve.dx, secondLeftCurve.dy,
-        secondRightCurve.dx, secondRightCurve.dy);
-
+    path.quadraticBezierTo(
+      secondLeftCurve.dx,
+      secondLeftCurve.dy,
+      secondRightCurve.dx,
+      secondRightCurve.dy,
+    );
 
     final thirdLeftCurve = Offset(size.width, size.height - 20);
     final thirdRightCurve = Offset(size.width, size.height);
-    path.quadraticBezierTo(thirdLeftCurve.dx, thirdLeftCurve.dy,
-        thirdRightCurve.dx, thirdRightCurve.dy);
+    path.quadraticBezierTo(
+      thirdLeftCurve.dx,
+      thirdLeftCurve.dy,
+      thirdRightCurve.dx,
+      thirdRightCurve.dy,
+    );
 
     path.lineTo(size.width, 0);
     path.close();
